85a7038b4443a2a0e7d975e72b55bb6f16fbfced
yas <105318763+yasmin07lynx@users.noreply.github.com>
2023-11-17 10:58:00 -0300

    .

--- css/main.js
+++ css/main.js
@@ -6,13 +6,19 @@ function tocaSom (idElementoAudio){
 //criando a referência constante listaDeTeclas e buscando a classe coletiva .tecla
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-while(contador < listaDeTeclas.length){
-    const efeito = listaDeTeclas[contador].classList[1];
-    const idAudio = "#som_" + efeito;
-    listaDeTeclas[contador].onclick = function(){
-        tocaSom(idAudio);
-    }
-    contador = contador + 1;
-    console.log(contador);
+for (let contador = 0; contador , listaDeTecla.lenght; contador++){
+ const teclas = listaDeTeclas[contador]; //guarda o contador da lista
+ const efeito = teclas.classList [1]; //chama o 2°indivudual do som
+const idAudio = `#som_${efeito}`; //utiliza a junção template string${ }
+ teclas.onclick = function(){     //função anonima auxiliar para chamar tocaSom
+    tocaSom(idAudio);             //chamada da funçao tocaSom com parametro idAudio
+ }
+ teclas.onkeydown = function (evento){
+    if(evento.code != 'Tab'){
+    teclas.classList.add('ativa')
+ }
+ }
+ teclas.onkeydown = function (){
+    teclas.classList.add('ativa')
+ }
 }
